bc5eb844d3a9c5b7059655f26cb17cb7e78fbebe
WHJabhi <88318991+WHJabhi@users.noreply.github.com>
2022-02-26 17:04:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ leftWristX = 0;
   canvas = createCanvas(550, 550);
   canvas.position(560,150);
 
-  poseNet=ml5.poseNet(modelLoaded);
+  poseNet=ml5.poseNet(video,modelLoaded);
   poseNet.on('pose',gotPoses)
 
 }
